4a6903574d67896b885aa49d5e429eae709f831b
chixx.eth <44033048+Chinoiserie1@users.noreply.github.com>
2022-08-15 11:29:30 +0200

    upgrade script chixx.eth

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ var ur = csv.toObjects(urs);
 var basicEgg = csv.toObjects(basicEggs);
 var basic = csv.toObjects(basics);
 var snapshot = csv.toObjects(snapshotTimeout);
-console.log(basics.length);
+
+// console.log(basic.length);
+// console.log(basicEgg.length);
+// console.log(ur.length);
+// console.log(urEgg.length);
+// console.log(legendaire.length);
+// console.log(basic.length + basicEgg.length + ur.length + urEgg.length + legendaire.length);
 
 const coupon = getCoupon("0x5E5871153763645D552Ba5D9082597f3be3D0788", 0, 0, 0, 0, CouponTypeEnum.PrivateSales);
 // console.log(coupon);
@@ -52,14 +58,14 @@ for (let i = 0; i < digest.length; ++i) {
   var obj = {};
   obj.owner = digest[i].owner;
   obj.legendaire = 0;
-  obj.urEgg = 0;
   obj.ur = 0;
-  obj.basicEgg = 0;
   obj.basic = 0;
+  obj.egg = 0;
   res.push(obj);
   for (let y = 0; y < digest[i].tokenId.length; y++) {
     for (let z = 0; z < legendaire.length; z++) {
       if (digest[i].tokenId[y] == legendaire[z].Legendaires) {
+        counter2++;
         res[i].legendaire++;
       }
     }
@@ -67,7 +73,9 @@ for (let i = 0; i < digest.length; ++i) {
   for (let y = 0; y < digest[i].tokenId.length; y++) {
     for (let z = 0; z < urEgg.length; z++) {
       if (digest[i].tokenId[y] == urEgg[z].URegg) {
-        res[i].urEgg++;
+        res[i].ur++;
+        res[i].egg++;
+        counter2++;
       }
     }
   }
@@ -75,24 +83,38 @@ for (let i = 0; i < digest.length; ++i) {
     for (let z = 0; z < ur.length; z++) {
       if (digest[i].tokenId[y] == ur[z].UR) {
         res[i].ur++;
+        counter2++;
       }
     }
   }
   for (let y = 0; y < digest[i].tokenId.length; y++) {
     for (let z = 0; z < basicEgg.length; z++) {
       if (digest[i].tokenId[y] == basicEgg[z].BasicEgg) {
-        res[i].basicEgg++;
+        res[i].basic++;
+        res[i].egg++;
+        counter2++;
       }
     }
   }
   for (let y = 0; y < digest[i].tokenId.length; y++) {
     for (let z = 0; z < basic.length; z++) {
       if (digest[i].tokenId[y] == basic[z].Basic) {
-        counter2++;
         res[i].basic++;
+        counter2++;
       }
     }
   }
 }
-console.log(res);
+
+// console.log(res);
 console.log(counter2);
+
+var privateWLCoupon = [];
+for (let i = 0; i < res.length; ++i) {
+  var obj = {};
+  obj.address = res[i].owner;
+  obj.coupon = getCoupon(res[i].owner, res[i].basic, res[i].ur, res[i].legendaire, res[i].egg, CouponTypeEnum.PrivateSales);
+  privateWLCoupon.push(obj);
+}
+
+console.log(privateWLCoupon);
